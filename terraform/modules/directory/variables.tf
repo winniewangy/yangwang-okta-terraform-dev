398a46b3bcb65e57ae051b0e4f03da16c8a963ff
Charles <oceanwangy@gmail.com>
2024-11-30 00:18:00 +1100

    er

--- terraform/modules/directory/variables.tf
+++ terraform/modules/directory/variables.tf
@@ -5,3 +5,12 @@ variable "okta_group_name" {
 variable "auth_server_name" {
   type = string
 }
+variable "user_first_name" {
+  type = string
+}
+variable "user_last_name" {
+  type = string
+}
+variable "user_login_email" {
+  type = string
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -40,3 +40,12 @@ variable "okta_group_name" {
 variable "auth_server_name" {
   type = string
 }
+variable "user_first_name" {
+  type = string
+}
+variable "user_last_name" {
+  type = string
+}
+variable "user_login_email" {
+  type = string
+}
